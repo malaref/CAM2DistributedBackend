cf25124dc676b878e6f7af481ff45565c2ff95af
Muhammad H. Al-Aref <muhammad.alaref96@eng-st.cu.edu.eg>
2017-10-13 17:43:31 +0200

    Cluster setup automation added!

--- setup.py
+++ setup.py
@@ -16,4 +16,10 @@ setup(
 		[console_scripts]
 		CAM2DistributedBackend=CAM2DistributedBackend:cli
 	''',
+	scripts=[
+		'bin/CAM2StartManager',
+		'bin/CAM2StartWorker',
+		'bin/CAM2StopManager',
+		'bin/CAM2StopWorker',
+	],
 ) 
